2ffc4320a5deef16f9734583fc82fd8b2b306d15
badri <badri.ikkurthy@gmail.com>
2024-07-04 15:43:53 +0530

    Updated Code

--- src/main/java/com/hackathon/fundtransfer/entity/FundTransfer.java
+++ src/main/java/com/hackathon/fundtransfer/entity/FundTransfer.java
@@ -1,8 +1,6 @@
 package com.hackathon.fundtransfer.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
